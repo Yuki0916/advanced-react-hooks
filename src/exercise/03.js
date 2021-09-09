66e14d3110b13f9fe44b43b5db094cfce4f75dcf
Jhu, Sin-Han <blueice0916@gmail.com>
2021-09-10 01:35:28 +0800

    feat(03.js): 實作useContext的錯誤處理，以及將邏輯抽出成共用function以便於重複使用

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -5,18 +5,28 @@ import {useContext, createContext, useState} from 'react'
 
 const CountContext = createContext()
 
+function useCount() {
+  const context = useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must to be used within the CountProvider')
+  }
+  return context
+} // 將useContext抽出，並且添加錯誤處理。將undefined白畫面問題有更細節的描述，並且可以與CountProvider封裝成模組
+
 function CountProvider({...props}) {
   const [count, setCount] = useState(0)
   return <CountContext.Provider value={[count, setCount]} {...props} />
 } // Kent C Dodds的建議 這樣就可以將資料狀態建立這個元件之中
+// 若封裝成模組
+// 舉例 import {useCount, CountProvider} from '../customHook/CountContext'
 
 function CountDisplay() {
-  const [count] = useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = useContext(CountContext) // 只取array第二個setCount function
+  const [, setCount] = useCount() // 只取array第二個setCount function
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
