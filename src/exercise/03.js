2f314da9fc2fd58a1132f2636ce73a37716822d9
Jhu, Sin-Han <blueice0916@gmail.com>
2021-09-10 01:01:03 +0800

    feat(03.js): 根據Kent C Dodds的設計，嘗試建立客製化的CountProvidere元件

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -5,26 +5,30 @@ import {useContext, createContext, useState} from 'react'
 
 const CountContext = createContext()
 
-function CountDisplay() {
-  const {count} = useContext(CountContext)
+function CountProvider({...props}) {
+  const [count, setCount] = useState(0)
+  return <CountContext.Provider value={[count, setCount]} {...props} />
+} // Kent C Dodds的建議 這樣就可以將資料狀態建立這個元件之中
 
+function CountDisplay() {
+  const [count] = useContext(CountContext)
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const {setCount} = useContext(CountContext)
+  const [, setCount] = useContext(CountContext) // 只取array第二個setCount function
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 比起自己的版本，App這裡不用建立count資料
   return (
     <div>
-      <CountContext.Provider value={{count, setCount}}>
+      <CountProvider>
         <CountDisplay />
         <Counter />
-      </CountContext.Provider>
+      </CountProvider>
     </div>
   )
 }
